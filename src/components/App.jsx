640f1643cf0f9644b42f7033c619af6308978c59
Nuruddin <nuruddinbinhoda@gmail.com>
2023-11-16 18:23:12 +0600

    Add: vans page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import StyledHeader from "./styles/Header.styles";
 import StyledHome from "./styles/Home.styles";
 import StyledAbout from "./styles/About.styles";
+import StyledVans from "./styles/Vans.styles";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<StyledHome />} />
         <Route path="about" element={<StyledAbout />} />
+        <Route path="vans" element={<StyledVans />} />
       </Routes>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ export default function Header({ className }) {
       </Link>
       <nav>
         <Link to="about">About</Link>
+        <Link to="vans">Vans</Link>
       </nav>
     </header>
   );
