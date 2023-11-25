3245bae8afd2b9dd69c69dbeaaf1cfd6a1a7a4aa
Nuruddin <nuruddinbinhoda@gmail.com>
2023-11-25 17:19:31 +0600

    edit: add loading in host vans

--- src/components/pages/HostVans.jsx
+++ src/components/pages/HostVans.jsx
@@ -1,32 +1,40 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { Link, useLoaderData, defer, Await } from "react-router-dom";
 import { getHostVans } from "../../../api";
 import { requireAuth } from "../../../utils";
 
 export async function loader() {
   await requireAuth();
-  return getHostVans();
+  return defer({ hostVans: getHostVans() });
 }
 
 export default function HostVans({ className }) {
-  const vans = useLoaderData();
+  const hostVansPromise = useLoaderData();
 
-  const hostVansEls = vans.map((van) => (
-    <Link to={van.id} key={van.id} className="host-van-link-wrapper">
-      <div className="host-van-single" key={van.id}>
-        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-        <div className="host-van-info">
-          <h3>{van.name}</h3>
-          <p>${van.price}/day</p>
+  function renderHostVanElements(vans) {
+    return vans.map((van) => (
+      <Link to={van.id} key={van.id} className="host-van-link-wrapper">
+        <div className="host-van-single" key={van.id}>
+          <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+          <div className="host-van-info">
+            <h3>{van.name}</h3>
+            <p>${van.price}/day</p>
+          </div>
         </div>
-      </div>
-    </Link>
-  ));
+      </Link>
+    ));
+  }
 
   return (
     <section className={className}>
       <h1 className="host-vans-title">Your listed vans</h1>
+
       <div className="host-vans-list">
-        <section>{hostVansEls}</section>
+        <Suspense fallback={<h2>Loading...</h2>}>
+          <Await resolve={hostVansPromise.hostVans}>
+            {renderHostVanElements}
+          </Await>
+        </Suspense>
       </div>
     </section>
   );
